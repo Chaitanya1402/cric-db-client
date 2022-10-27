339974596f80d71d9dddb8aa3e3375b6ab1c65cc
Chaitanya <chaitanyas2002@gmail.com>
2022-10-27 22:27:03 +0530

    navbar responsiveness and theming added in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
 import { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import ThemeContext from '../contexts/ThemeContext';
 
 const Navbar = (props) => {
   const themeInfo = useContext(ThemeContext);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleNavbarToggle = () => {
+    if (!isExpanded) {
+      setIsExpanded(true);
+      document.getElementById('navbar').style.display = "block";
+    }
+    else {
+      setIsExpanded(false);
+      document.getElementById('navbar').style.display = "none";
+    }
+  }
 
   useEffect(() => {
     let slider = document.getElementById('slider');
@@ -13,16 +26,18 @@ const Navbar = (props) => {
   
   return (
     <div className='bg-purple-300 text-black'>
-      <div className='w-[85%] h-16 flex m-auto justify-between items-center'>
+      <div className='w-[85%] h-16 flex m-auto justify-between items-center relative'>
         <div>
           <h2 className='text-2xl font-medium font-anton text-black'>CricDB</h2>
         </div>
-        <div> 
-          <ul className='flex justify-center items-center font-lato'>
-            <li className='mx-3'><Link className='px-2 py-1 text-lg rounded-xl border-2 border-purple-300 hover:bg-purple-200 link-transition' to={'/'}>Home</Link></li>
-            <li className='mx-3'><Link className='px-2 py-1 text-lg rounded-xl border-2 border-purple-300 hover:bg-purple-200 link-transition' to={'/about'}>About</Link></li>
-            <li className='mx-3'><Link className='px-2 py-1 text-lg rounded-xl border-2 border-purple-300 hover:bg-purple-200 link-transition' to={'/contact'}>Contact</Link></li>
+        <div className='block md:hidden absolute right-0 text-2xl' onClick={handleNavbarToggle}>{isExpanded === false ? <i class="fa-solid fa-bars"></i> : <i class="fa-solid fa-xmark"></i>}</div>
+        <div className='hidden md:block' id='navbar'> 
+          <ul className='navbar-ul flex justify-center items-center font-lato flex-col md:flex-row absolute md:static top-[64px] right-0 md:bg-transparent py-3 w-[70%] md:w-auto z-30'>
+            <li className='md:mx-3 p-1 md:py-0 w-[100%]'><Link className='p-1 my-1 md:px-2 md:py-1 text-lg rounded-md md:rounded-xl border border-purple-300 md:hover:bg-purple-200 md:bg-transparent link-transition w-[90%] block m-auto md:inline text-center' to={'/'}>Home</Link></li>
+            <li className='md:mx-3 p-1 md:py-0 w-[100%]'><Link className='p-1 my-1 md:px-2 md:py-1 text-lg rounded-md md:rounded-xl border border-purple-300 md:hover:bg-purple-200 md:bg-transparent link-transition w-[90%] block m-auto md:inline text-center' to={'/about'}>About</Link></li>
+            <li className='md:mx-3 p-1 md:py-0 w-[100%]'><Link className='p-1 my-1 md:px-2 md:py-1 text-lg rounded-md md:rounded-xl border border-purple-300 md:hover:bg-purple-200 md:bg-transparent link-transition w-[90%] block m-auto md:inline text-center' to={'/contact'}>Contact</Link></li>
             {/* <button onClick={() => {props.toggleTheme()}}>Switch to {props.theme === "light" ? "Dark" : "Light"} mode</button> */}
+            <li className='mt-8 md:mt-2 md:mx-3 p-1 py-2 md:py-0 my-1'>
             <label id="switch" className="switch">
             <input type="checkbox" onChange={() => {
               props.toggleTheme();
@@ -30,7 +45,8 @@ const Navbar = (props) => {
             }} 
               id="slider" />
             <span className="slider round"></span>
-        </label>
+            </label>
+            </li>
           </ul>
         </div>
       </div>
