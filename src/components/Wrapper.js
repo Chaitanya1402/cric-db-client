e5c1ab42f520dd03162377680dce412a8bf2a2a9
Chaitanya <chaitanyas2002@gmail.com>
2022-10-27 19:27:14 +0530

    added styles to body tag

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,11 +1,23 @@
 import { useLocation } from 'react-router-dom';
-import { useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect, useEffect } from 'react';
+import ThemeContext from '../contexts/ThemeContext';
 
 const Wrapper = ({ children }) => {
     const location = useLocation();
+    const themeInfo = useContext(ThemeContext);
     useLayoutEffect(() => {
         document.documentElement.scrollTo(0, 0);
     }, [location.pathname]);
+
+    useEffect(() => {
+        document.body.style.transition = "all 0.3s linear";
+        if (themeInfo.theme === "light") {
+            document.body.style.backgroundColor = "#fff";
+        }
+        else {
+            document.body.style.backgroundColor = "#18181b";
+        }
+    }, [themeInfo.theme])
     return children
 }
 
